6000470a498b50f3288a4a3dd7eb1bc3f1c00d51
Andrew Plotkin <erkyrath@eblong.com>
2015-01-26 17:23:24 -0500

    Put the empty-block check back in.

--- csscons.c
+++ csscons.c
@@ -203,7 +203,7 @@ static void construct_rulesets(mincss_context *context, node *nod, stylesheet *s
         node *blocknod = nod->nodes[blockpos];
         construct_declarations(context, blocknod, rgrp);
 
-        if (0 /*###!rgrp->numselectors || !rgrp->numdeclarations###*/) { /*### temporarily suppress this check */
+        if (!rgrp->numselectors || !rgrp->numdeclarations) {
             /* Empty, skip. */
             rulegroup_delete(rgrp);
         }
